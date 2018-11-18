575e032ddfcf6fb1bfcd63d1a971932aa6962d49
Roj <Roj@users.noreply.github.com>
2018-11-18 03:36:41 -0300

    Y_[e][i] -> Y_EQ[e, i] y otros subindices

--- generar_modelos.py
+++ generar_modelos.py
@@ -10,16 +10,16 @@ parametros: Calidad_T{d in deporte}, Calidad_E{d in deporte},
             Calidad_F{d in deporte}. Especialista{i in equipo, d in deporte},
             Cubre{i in equipo, d in deporte}
 
-variables:  Y_{e in Eventos}, Y_{e in Eventos, i in Equipos},
+variables:  Y_{e in Eventos}, Y_EQ{e in Eventos, i in Equipos},
             T_{e in Eventos}_1, T_{e in Eventos}_2,
             E{i in Equipos, s in Sedes, j in Jornadas}
 
-restricc.:  * forall e in Eventos: sum{i in Equipos} Y_e_i <= 1
-            * forall e in Eventos: sum{i in Equipos} Y_e_i = Y_e
-            * ("forall d in Deportes") forall i in Equipos, e in EventosDeporte_d: Y_e_i <= c_i_d
+restricc.:  * forall e in Eventos: sum{i in Equipos} Y_EQ_e_i <= 1
+            * forall e in Eventos: sum{i in Equipos} Y_EQ_e_i = Y_e
+            * ("forall d in Deportes") forall i in Equipos, e in EventosDeporte_d: Y_EQ_e_i <= c_i_d
             * conjuntos maximales como una rest. por cada conj.
             * restricciones separadas por cada sede y jornada:
-                  forall i: E_i_s_j <= Y_e_i + Y_e'_i + ... <= n * E_i_s_j
+                  forall i: E_i_s_j <= Y_EQ_e_i + Y_EQ_e'_i + ... <= n * E_i_s_j
                   n = cantidad de eventos para esa sede en esa jornada
                       (i.e. terminos de la suma)
             * forall i in Equipos, j in Jornadas: sum{s in Sedes} E_i_s_j <= 1
@@ -27,11 +27,11 @@ restricc.:  * forall e in Eventos: sum{i in Equipos} Y_e_i <= 1
                   Y_final <= Y_e + Y_e' + Y_e'' + ...
                   (e, e', e'', ... son del mismo deporte que final y están
                   antes)
-            * ("forall d in Deportes") forall e in Final_d, i in Equipos: Y_e_i <= Especialista_i_d
+            * ("forall d in Deportes") forall e in Final_d, i in Equipos: Y_EQ_e_i <= Especialista_i_d
             * forall e in Eventos: Y_e = T_e_1 + T_e_2
             * conjuntos maximales para exclusividad de transmision
 
-funcional:  sum {d in deportes}(sum{e in EventosDeporte_d} (Y_e_i(Calidad_T_d + Calidad_E_d * Especialista_i_d))
+funcional:  sum {d in deportes}(sum{e in EventosDeporte_d} (Y_EQ_e_i(Calidad_T_d + Calidad_E_d * Especialista_i_d))
                                 + sum{e in Final_d} Y_e * Calidad_F_d)
 
 """
@@ -174,20 +174,20 @@ print("set Canales;")
 print("param Calidad_T{d in Deportes};")
 print("param Calidad_E{d in Deportes};")
 print("param Calidad_F{d in Deportes};")
-print("param Especialista_{i in Equipos, d in Deportes};")
+print("param Especialista{i in Equipos, d in Deportes};")
 print("param Cubre_{i in Equipos, d in Deportes};")
 
 # Definición de variables
 print("var Y_{e in Eventos}, binary;")
-print("var Y_{e in Eventos, i in Equipos}, binary;")
+print("var Y_EQ_{e in Eventos, i in Equipos}, binary;")
 print("var T_{e in Eventos, c in Canales}, binary;")
-print("var E_{i in Equipos, s in Sedes, j in Jornada}, binary;")
+print("var E_{i in Equipos, s in Sedes, j in Jornadas}, binary;")
 
 # Definición de funcional
 terminos = ""
 for deporte in deportes:
     terminos += "\t+ "
-    terminos += "sum{{e in EventosDeporte{0}, i in Equipos}}(Y_[e][i](Calidad_T[\"{0}\" + Calidad_E[\"{0}\"] * Especialista[i][\"{0}\"]))".format(deporte)
+    terminos += "sum{{e in EventosDeporte{0}, i in Equipos}}(Y_EQ_[e, i]*(Calidad_T[\"{0}\" + Calidad_E[\"{0}\"] * Especialista[i, \"{0}\"]))".format(deporte)
     terminos += " + sum{{e in FinalDeporte{0}}} (Y_[e] * Calidad_F[\"{0}\"])\n".format(deporte)
 print("maximize z: {0};".format(terminos[3:]))
 
@@ -195,12 +195,12 @@ print("maximize z: {0};".format(terminos[3:]))
 
 # La agrego pero comentada porque esta restricción es redundante
 # si las siguientes restricciones hacen bivalente = suma de bivalentes
-# (Y_e = Y_e_1 + ...)
+# (Y_e = Y_EQ_e_1 + ...)
 #print("s.t. max_1_eq_ev_{e in Eventos}: sum{i in Equipos}(Y_[e][i]) <= 1;")
-print("s.t. def_Y_{e in Eventos}: Y_[e] = sum{i in Equipos}Y_[e][i];")
+print("s.t. def_Y_{e in Eventos}: Y_[e] = sum{i in Equipos}Y_EQ_[e][i];")
 for deporte in deportes:
     print(("s.t. cubr_eq_dep_{0}{{i in Equipos, e in EventosDeporte{0}}}:"
-        + "Y_[e][i] <= Cubre_[i][\"{0}\"];").format(deporte))
+        + "Y_EQ_[e][i] <= Cubre_[i][\"{0}\"];").format(deporte))
 
 for sede in sedes:
     for jornada in jornadas:
@@ -219,7 +219,7 @@ print("s.t. excl_E_i_s_j{i in Equipos, j in Jornadas}: "
 
 for deporte in deportes:
     print(("s.t. final_especialista_dep{0}_{{e in FinalDeporte{0}, i in Equipos}}: "
-        + "Y_[e][i] <= Especialista_[i][{0}];").format(deporte))
+        + "Y_EQ_[e][i] <= Especialista[i, \"{0}\"];").format(deporte))
 
 print("s.t. transmision{e in Eventos}: Y_[e] = sum{c in Canales} T_[e][c];")
 
